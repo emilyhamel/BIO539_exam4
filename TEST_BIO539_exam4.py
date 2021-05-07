9a7192eff9ef15eeb86d257a04b25a29e0a753b0
Emily Hamel <em.jhamel@gmail.com>
2021-05-06 21:21:48 -0400

    test file psb and obs outline

---
+++ TEST_BIO539_exam4.py
@@ -0,0 +1,20 @@
+from BIO539_exam4 import *
+
+#Test of the function to determine possible k-mers
+def test_psb_kmers():
+  k = 
+  seq = ''
+  actual_result = psb_kmers(k, seq)
+  expected_result =
+  assert actual_result == expected_result
+  
+#Test of the function to determine observed k-mers
+def test_obs_kmers():
+  k =
+  seq = ''
+  actual_result = obs_kmers(k, seq)
+  expected_result =
+  assert actual_result == expected_result
+  
+  
+  
